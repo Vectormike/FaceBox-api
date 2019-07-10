1571187c1b724742e59f87b20a2eb982b30017dd
vectormike40 <victorjonah199@gmail.com>
2019-07-10 13:46:29 +0100

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
-import Middleware from '../middleware/middleware';
 
-router.post('/register', Middleware.authorize)
+// import Middleware from '../middleware/middleware';
+import * as Controller from '../controllers/userController';
+
+router.post('/register', Controller.register)
 
 export default router;
