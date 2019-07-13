9bd9d38ed9d0c6e8e8279eb678243c9e53c5d200
vectormike40 <victorjonah199@gmail.com>
2019-07-13 12:24:45 +0100

    check if userexists

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,34 +2,49 @@ import knex from 'knex';
 import debug from 'debug';
 const deb = debug('app:app.register')
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host : '127.0.0.1',
-        user : 'facebox',
-        password : '0000',
-        database : 'facebox'
-    }
+const database = knex({
+	  client: 'pg',
+	  connection: {
+			host : '127.0.0.1',
+			user : 'facebox',
+			password : '0000',
+			database : 'facebox'
+	  }
 });
 
 class userController{
-        static register(req, res) {
-            const { name, email, password} = req.body;
-            db('users').insert({
-                name: name,
-                email: email,
-                joined: new Date()
-            })
-            .then(user => {
-                res.status(201).json({
-                    message: 'User Created',
-                    user: {
-                        name: user.name,
-                        email: user.email
-                }      
-            })
-        })
-    }
+			static register(req, res) {
+				  const { name, email, password} = req.body;
+
+				  // Check if user exists with email else create account
+				  database('users')
+						.where({ email: email })
+						.then(user => {
+							  if(user) {
+									res.status(409).json({
+										  message: 'User exists',
+										  code: 409
+									})
+							  } else {
+									database('users')
+									.insert({
+										  name: name,
+										  email: email,
+										  joined: new Date()
+									})
+									.then(user => {
+												res.status(201).json({
+													  message: 'User Created',
+													  user: {
+															name: user.name,
+															email: user.email
+														 }
+												})
+											})
+										}
+									})
+
+	  }
 }
 
 export default userController;
