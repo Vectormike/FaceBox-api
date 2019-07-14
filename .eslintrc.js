9037be55e51b3358618d3267ffa529e8088c19cf
vectormike40 <victorjonah199@gmail.com>
2019-07-14 20:20:10 +0100

    😗

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,19 @@
-module.exports = { "extends": "plugin:prettier/recommended" };
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "airbnb", "prettier"
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
